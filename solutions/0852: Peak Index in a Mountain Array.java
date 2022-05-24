bc83108a55ad201732f27cfa25dbb536fa8728c3
Daniel Al-Khrysat <gusalkda@student.gu.se>
2022-05-24 23:28:56 +0200

    Create 0852: Peak Index in a Mountain Array.java

---
+++ solutions/0852: Peak Index in a Mountain Array.java
@@ -0,0 +1,34 @@
+/*
+Let's call an array arr a mountain if the following properties hold:
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... arr[i-1] < arr[i]
+arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+Given an integer array arr that is guaranteed to be a mountain, return any i such that arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+
+Example 1:
+Input: arr = [0,1,0]
+Output: 1
+
+Example 2:
+Input: arr = [0,2,1,0]
+Output: 1
+
+Example 3:
+Input: arr = [0,10,5,2]
+Output: 1
+
+Constraints:
+3 <= arr.length <= 104
+0 <= arr[i] <= 106
+arr is guaranteed to be a mountain array.
+*/
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        for (int i = 1; i<arr.length; i++) {          //iterate arr
+            if (arr[i] < arr[i-1]) return i-1;        //if current array index is smaller than the previous one, then the previous one was the peak and we return it
+        }
+        return 0;                                     //none found, return 0
+    }
+}
